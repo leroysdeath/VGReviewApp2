9eea0a3f538c03f723ee74272fba433812c2413b
leroysdeath <leroysdeath@gmail.com>
2025-07-08 01:22:08 -0400

    Fix preview breaking issues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,17 +25,19 @@ export const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <Link to="/" className="flex items-center space-x-2">
-              <Gamepad2 className="h-8 w-8 text-purple-400" />
-              <span className="text-xl font-bold text-white">GameVault</span>
-            </Link>
-          </div>
-
-          {/* Search Bar */}
-          <div className="hidden md:block flex-1 max-w-lg mx-8">
-            <form onSubmit={handleSearch} className="relative">
-              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
-              <input
+            <div className="flex items-center space-x-4">
+              <Link to="/" className="flex items-center space-x-2">
+                <Gamepad2 className="h-8 w-8 text-purple-400" />
+                <span className="text-xl font-bold text-white">GameVault</span>
+              </Link>
+              <Link
+                to="/dummy-game"
+                className="p-2 bg-purple-600 rounded-lg hover:bg-purple-700 transition-colors"
+                title="Dummy Test Game"
+              >
+                <TestTube className="h-5 w-5 text-white" />
+              </Link>
+            </div>
                 type="text"
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
