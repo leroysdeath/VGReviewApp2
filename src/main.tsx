ebcdb8f53440680186da07be1373e0e552ead42c
leroysdeath <leroysdeath@gmail.com>
2025-07-23 19:39:07 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
+// src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './styles/accessibility.css';
+import './utils/envCheck'; // Validate environment on startup
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
