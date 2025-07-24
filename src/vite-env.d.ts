e86c110335ab1acfdfe28e19d3b343c06a6af502
leroysdeath <leroysdeath@gmail.com>
2025-07-23 23:45:55 -0400

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="vite/client" />
+a// src/types/env.d.ts
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_ANON_KEY: string
+  readonly VITE_APP_ENV: string
+  readonly VITE_APP_URL: string
+  readonly TWITCH_CLIENT_ID: string
+  readonly TWITCH_APP_ACCESS_TOKEN: string
+  readonly VITE_GOOGLE_ANALYTICS_ID?: string
+  readonly VITE_SENTRY_DSN?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+declare const __APP_VERSION__: string;
