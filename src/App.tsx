853c814679846dd9fe3e0aa918c52db73d8823c6
leroysdeath <leroysdeath@gmail.com>
2025-07-23 20:50:57 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ErrorBoundary } from './components/ErrorBoundary';
+import { AuthProvider } from './context/AuthProvider';
 import { ResponsiveNavbar } from './components/ResponsiveNavbar';
 import { ResponsiveLandingPage } from './components/ResponsiveLandingPage';
 import { ReviewProvider } from './context/ReviewContext';
-import { AuthModalProvider } from './context/AuthModalContext';
-import { AuthProvider } from './context/AuthProvider';
 import { GamePage } from './pages/GamePage';
 import { GameSearchPage } from './pages/GameSearchPage';
 import { SearchResultsPage } from './pages/SearchResultsPage';
@@ -24,36 +23,30 @@ function App() {
     <HelmetProvider>
       <ErrorBoundary>
         <AuthProvider>
-          <AuthModalProvider>
-            <ReviewProvider currentUserId={1}>
-              <Router>
-                <div className="min-h-screen bg-gray-900">
-                  <SEOHead />
-                  <ResponsiveNavbar />
-                  <Routes>
-                    <Route path="/" element={<ResponsiveLandingPage />} />
-                    <Route path="/game/:id" element={<GamePage />} />
-                    
-                    {/* Search routes */}
-                    <Route path="/search" element={<SearchResultsPage />} />
-                    <Route path="/search-results" element={<SearchResultsPage />} />
-                    
-                    {/* User routes */}
-                    <Route path="/user/:id" element={<UserPage />} />
-                    <Route path="/users" element={<UserSearchPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/profile" element={<ProfilePage />} />
-                    
-                    {/* Review routes */}
-                    <Route path="/review/:gameId?" element={<ReviewFormPage />} />
-                    
-                    {/* Development routes */}
-                    {import.meta.env.DEV && <Route path="/igdb-test" element={<IGDBTestPage />} />}
-                  </Routes>
-                </div>
-              </Router>
-            </ReviewProvider>
-          </AuthModalProvider>
+          <ReviewProvider currentUserId={1}>
+            <Router>
+              <div className="min-h-screen bg-gray-900">
+                <SEOHead />
+                <ResponsiveNavbar />
+                <Routes>
+                  <Route path="/" element={<ResponsiveLandingPage />} />
+                  <Route path="/game/:id" element={<GamePage />} />
+                  
+                  {/* UPDATED: Both search routes now point to SearchResultsPage */}
+                  <Route path="/search" element={<SearchResultsPage />} />
+                  <Route path="/search-results" element={<SearchResultsPage />} />
+                  
+                  {/* Keep your other existing routes */}
+                  <Route path="/user/:id" element={<UserPage />} />
+                  <Route path="/users" element={<UserSearchPage />} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/review/:gameId?" element={<ReviewFormPage />} />
+                  <Route path="/profile" element={<ProfilePage />} />
+                  {import.meta.env.DEV && <Route path="/igdb-test" element={<IGDBTestPage />} />}
+                </Routes>
+              </div>
+            </Router>
+          </ReviewProvider>
         </AuthProvider>
       </ErrorBoundary>
     </HelmetProvider>
