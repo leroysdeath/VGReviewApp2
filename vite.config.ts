7ef3e3310f57c328733f8725734c046cda35eef0
leroysdeath <leroysdeath@gmail.com>
2025-08-21 17:44:17 -0400

    Enable source maps for debugging production errors

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     target: 'es2015',
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false,
+    sourcemap: true,
     minify: 'terser',
     rollupOptions: {
       output: {
